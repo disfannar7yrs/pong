10751329f855100ad22b5ebb6d57a98a27a06855
fannar <edlifannarra@gmail.com>
2022-03-08 04:30:07 +0000

    first commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+local player = require "player"
+local ai = require "ai"
+local ball = require "ball"
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
